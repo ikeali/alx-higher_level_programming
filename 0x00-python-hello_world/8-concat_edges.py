f512ed62eb4e18416653cf9a20593901ce921e61
ikeali <ikechukwualisigwe01@mail.com>
2023-11-28 13:26:55 -0800

    all

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-        language that combines remarkable power with\
-        very clear syntax"
-print(str[str.find("object"):str.rfind("object") + 6] + "\n")
-
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[115:127] + str[0:6]
+print(str)
