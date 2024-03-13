17216d80af9b6345219eaf58a04ac12f05f18006
ikeali <ikechukwualisigwe01@mail.com>
2024-03-13 02:55:49 -0700

    0-rectangle.js

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,14 +1,2 @@
 #!/usr/bin/node
-
-// eslint-disable-next-line semi
-'use strict';
-
-// Define Rectangle class
-class Rectangle {}
-
-// Print a Rectangle instance
-console.log(new Rectangle());
-
-// Print the constructor of the Rectangle instance
-console.log('[Class: Rectangle]');
-
+module.exports = class Rectangle {};
